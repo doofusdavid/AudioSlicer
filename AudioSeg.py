1e1929e8be96508a041fef47b2c0ccf62202a228
David Edwards <david.edwards@gmail.com>
2023-11-12 13:35:11 -0700

    break into main function

--- AudioSeg.py
+++ AudioSeg.py
@@ -9,13 +9,21 @@ from tqdm import tqdm
 import json
 from datetime import datetime, timedelta
 
-# Check if the correct number of arguments were provided
-if len(sys.argv) != 3:
-    print("Usage: {} <input_file> <output_file>".format(sys.argv[0]))
-    sys.exit(1)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+def main():
+    # Check if the correct number of arguments were provided
+    if len(sys.argv) != 3:
+        print("Usage: {} <input_file> <output_file>".format(sys.argv[0]))
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    split_audio(input_file, output_file)
+
+
+if __name__ == "__main__":
+    main()
+
 # Utility functions
 
 
@@ -69,94 +77,96 @@ def rising_edges(binary_signal):
         index += 1
 
 
-'''
-Last Acceptable Values
+def split_audio(input_file, output_file):
+    '''
+    Last Acceptable Values
 
-min_silence_length = 0.3
-silence_threshold = 1e-3
-step_duration = 0.03/10
+    min_silence_length = 0.3
+    silence_threshold = 1e-3
+    step_duration = 0.03/10
 
-'''
-# The minimum length of silence at which a split may occur [seconds]. Defaults to 3 seconds.
-min_silence_length = 0.6
-# The energy level (between 0.0 and 1.0) below which the signal is regarded as silent.
-silence_threshold = 1e-4
-# The amount of time to step forward in the input file after calculating energy. Smaller value = slower, but more accurate silence detection. Larger value = faster, but might miss some split opportunities. Defaults to (min-silence-length / 10.).
-step_duration = 0.03/10
+    '''
 
+    # The minimum length of silence at which a split may occur [seconds]. Defaults to 3 seconds.
+    min_silence_length = 0.6
+    # The energy level (between 0.0 and 1.0) below which the signal is regarded as silent.
+    silence_threshold = 1e-4
+    # The amount of time to step forward in the input file after calculating energy. Smaller value = slower, but more accurate silence detection. Larger value = faster, but might miss some split opportunities. Defaults to (min-silence-length / 10.).
+    step_duration = 0.03/10
 
-input_filename = input_file
-window_duration = min_silence_length
-if step_duration is None:
-    step_duration = window_duration / 10.
-else:
-    step_duration = step_duration
-
-output_filename_prefix = os.path.splitext(os.path.basename(input_filename))[0]
-dry_run = False
-
-print("Splitting {} where energy is below {}% for longer than {}s.".format(
-    input_filename,
-    silence_threshold * 100.,
-    window_duration
-)
-)
-
-# Read and split the file
-
-sample_rate, samples = input_data = wavfile.read(
-    filename=input_filename, mmap=True)
-
-max_amplitude = np.iinfo(samples.dtype).max
-print(max_amplitude)
-
-max_energy = energy([max_amplitude])
-print(max_energy)
-
-window_size = int(window_duration * sample_rate)
-step_size = int(step_duration * sample_rate)
-
-signal_windows = windows(
-    signal=samples,
-    window_size=window_size,
-    step_size=step_size
-)
-
-window_energy = (energy(w) / max_energy for w in tqdm(
-    signal_windows,
-    total=int(len(samples) / float(step_size))
-))
-
-window_silence = (e > silence_threshold for e in window_energy)
-
-cut_times = (r * step_duration for r in rising_edges(window_silence))
-
-# This is the step that takes long, since we force the generators to run.
-print("Finding silences...")
-cut_samples = [int(t * sample_rate) for t in cut_times]
-cut_samples.append(-1)
-
-cut_ranges = [(i, cut_samples[i], cut_samples[i+1])
-              for i in range(len(cut_samples) - 1)]
-
-video_sub = {str(i): [str(GetTime(((cut_samples[i])/sample_rate))),
-                      str(GetTime(((cut_samples[i+1])/sample_rate)))]
-             for i in range(len(cut_samples) - 1)}
-
-for i, start, stop in tqdm(cut_ranges):
-    output_file_path = "{}_{:03d}.wav".format(
-        os.path.join(output_dir, output_filename_prefix),
-        i
-    )
-    if not dry_run:
-        print("Writing file {}".format(output_file_path))
-        wavfile.write(
-            filename=output_file_path,
-            rate=sample_rate,
-            data=samples[start:stop]
-        )
+    input_filename = input_file
+    window_duration = min_silence_length
+    if step_duration is None:
+        step_duration = window_duration / 10.
     else:
-        print("Not writing file {}".format(output_file_path))
+        step_duration = step_duration
 
-with open(output_dir+'\\'+output_filename_prefix+'.json', 'w') as output:
-    json.dump(video_sub, output)
+    output_filename_prefix = os.path.splitext(
+        os.path.basename(input_filename))[0]
+    dry_run = False
+
+    print("Splitting {} where energy is below {}% for longer than {}s.".format(
+        input_filename,
+        silence_threshold * 100.,
+        window_duration
+    )
+    )
+
+    # Read and split the file
+
+    sample_rate, samples = input_data = wavfile.read(
+        filename=input_filename, mmap=True)
+
+    max_amplitude = np.iinfo(samples.dtype).max
+    print(max_amplitude)
+
+    max_energy = energy([max_amplitude])
+    print(max_energy)
+
+    window_size = int(window_duration * sample_rate)
+    step_size = int(step_duration * sample_rate)
+
+    signal_windows = windows(
+        signal=samples,
+        window_size=window_size,
+        step_size=step_size
+    )
+
+    window_energy = (energy(w) / max_energy for w in tqdm(
+        signal_windows,
+        total=int(len(samples) / float(step_size))
+    ))
+
+    window_silence = (e > silence_threshold for e in window_energy)
+
+    cut_times = (r * step_duration for r in rising_edges(window_silence))
+
+    # This is the step that takes long, since we force the generators to run.
+    print("Finding silences...")
+    cut_samples = [int(t * sample_rate) for t in cut_times]
+    cut_samples.append(-1)
+
+    cut_ranges = [(i, cut_samples[i], cut_samples[i+1])
+                  for i in range(len(cut_samples) - 1)]
+
+    video_sub = {str(i): [str(GetTime(((cut_samples[i])/sample_rate))),
+                          str(GetTime(((cut_samples[i+1])/sample_rate)))]
+                 for i in range(len(cut_samples) - 1)}
+
+    for i, start, stop in tqdm(cut_ranges):
+        output_file_path = "{}_{:03d}.wav".format(
+            os.path.join(output_dir, output_filename_prefix),
+            i
+        )
+        if not dry_run:
+            print("Writing file {}".format(output_file_path))
+            wavfile.write(
+                filename=output_file_path,
+                rate=sample_rate,
+                data=samples[start:stop]
+            )
+        else:
+            print("Not writing file {}".format(output_file_path))
+
+    with open(output_dir+'\\'+output_filename_prefix+'.json', 'w') as output:
+        json.dump(video_sub, output)
